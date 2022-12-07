b33999602a2dc179cd69f31fbc8160d14a75dd52
Hyunkyu Lee <hyunkyle@c4r9s2.42seoul.kr>
2022-12-07 20:12:18 +0900

    reserve todo

--- test.cpp
+++ test.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include "reverse_iterator.hpp"
 #include <vector>
-
+#include <limits>
 
 int main()
 {
- 
+	std::vector<long> v1;
+	std::cout << v1.max_size() << std::endl;
+	std::cout << (std::size_t)(std::numeric_limits<long>::max() - 1) << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -3,6 +3,8 @@
 
 #include <memory>
 #include "reverse_iterator.hpp"
+#include <stdexcept>
+#include <limits>
 
 namespace ft
 {
@@ -17,15 +19,11 @@ namespace ft
         typedef std::ptrdiff_t                          difference_type;
         typedef value_type&                             reference;
         typedef const value_type&                       const_reference;
-        typedef allocator_type::pointer                 pointer;
-        typedef allocator_type::const_pointer           const_pointer;
-        typedef value_type::iterator                    iterator;
-        typedef const value_type::iterator              const_iterator;
-        typedef ft::reverse_iterator<iterator>          reverse_iterator;
-        typedef ft::reverse_iterator<const_iterator>    const_reverse_iterator;
+        typedef typename allocator_type::pointer        pointer;
+        typedef typename allocator_type::const_pointer  const_pointer;
     
     private:
-        pointer         _array;
+        T *             _array;
         allocator_type  _alloc;
         size_type       _capacity;
         size_type       _c_idx;
@@ -33,19 +31,19 @@ namespace ft
     private:
         void _fill_array(size_type count, const_reference value)
         {
-            while (c_idx < count)
+            while (_c_idx < count)
             {
                 _array[_c_idx] = value;
-                c_idx++;
+                _c_idx++;
             }
         }
 
-        template <class InputIt = std::iterator<T> >
+        template <class InputIt>
         void _fill_array_iter(InputIt first, InputIt last)
         {
             while (first != last)
             {
-                _array[c_idx++] = *first;
+                _array[_c_idx++] = *first;
                 first++;
             }
         }
@@ -69,78 +67,160 @@ namespace ft
         :vector()
         {
             _alloc = allocator;
-            if (count > 0)
-            {
-                _array = _alloc.allocate(count);
-                _fill_array(count, value);
-            }
+            assign(count, value);
         }
     
-        template <class InputIt = std::iterator<T> >
+        template <class InputIt>
         vector(InputIt first, InputIt last, const allocator_type& allocator = Allocator())
         :vector()
         {
             _alloc = allocator;
-            size_type count = 0;
-            InputIt tmp = first;
-            while (tmp != last)
-            {
-                tmp++
-                count++;
-            }
-            _array = _alloc.allocate(count);
-            _fill_array_iter(first, last);
+            assign(first, last);
         }
 
         vector(const vector& other)
         {
-            _alloc = other._alloc;
-            _capacity = other._capacity;
-            _c_idx = 0;
-            _array = _alloc.allocate(_capacity);
-            _fill_array_iter(other.begin(), other.end());            
+            *this = other;            
         }
 
         vector(const vector& other, const allocator_type& alloc)
         {
             _alloc = alloc;
-            _capacity = other._capacity;
-            _c_idx = 0;
-            _array = _alloc.allocate(_capacity);
-            _fill_array_iter(other.begin(), other.end());            
-        }
-
-        vector(vector&& other)
-        {
-            _alloc = other._alloc;
-            _capacity = other._capacity;
-            _c_idx = other._c_idx;
-            _array = other._array;
-            other._array = NULL;
-        }
-
-        vector(vector&& other, const allocator_type& alloc)
-        {
-            _alloc = alloc;
-            _capacity = other._capacity;
-            _c_idx = other._c_idx;
-            _array = other._array;
-            other._array = NULL;
-        }
-
-        vector(std::initializer_list<T> init, const allocator_type& alloc = Allocator())
-        {
-            _alloc = alloc;
-            _capacity = init.size();
-            _c_idx = 0;
-            _array = _alloc.allocate(_capacity);
-            _fill_array_iter(init.begin(), init.end());
+            *this = other;           
         }
 
         ~vector()
         {
             _alloc.destroy(_array);
             _alloc.deallocate(_array, _capacity);
+        }
+
+        vector& operator=( const vector& other )
+        {
+            if (_array)
+            {
+                _alloc.destroy(_array);
+                _alloc.deallocate(_array, _capacity);
+            }
+            this->_c_idx = 0;
+            this->_capacity = other._capacity;
+            this->_array = _alloc.allocate(this->_capacity);
+            _fill_array_iter(other.begin(), other.end());
+        }
+        
+        void assign( size_type count, const T& value )
+        {
+            if (_array)
+            {
+                _alloc.destroy(_array);
+                _alloc.deallocate(_array, _capacity);
+            }
+            this->_c_idx = 0;
+            this->_capacity = count;
+            this->array = _alloc.allocate(this->_capacity);
+            _fill_array(count, value);
+        }
+
+        template< class InputIt >
+        void assign( InputIt first, InputIt last )
+        {
+            if (_array)
+            {
+                _alloc.destroy(_array);
+                _alloc.deallocate(_array, _capacity);
+            }
+            size_type count = 0;
+            InputIt tmp = first;
+            while (tmp != last)
+            {
+                tmp++;
+                count++;
+            }
+            _array = _alloc.allocate(count);
+            this->_c_idx = 0;
+            this->_capacity = count;
+            _fill_array_iter(first, last);
+        }
+
+        allocator_type get_allocator() const
+        {
+            return this->_alloc;
+        }
+
+        reference at( size_type pos )
+        {
+            if (pos >= this->_c_idx)
+                throw std::out_of_range("index out of range error");
+            return this->_array[pos];
+        }
+
+        const_reference at( size_type pos ) const
+        {
+            if (pos >= this->_c_idx)
+                throw std::out_of_range("index out of range error");
+            return this->_array[pos];
+        }
+
+        reference operator[]( size_type pos )
+        {
+            return this->_array[pos];
+        }
+
+        const_reference operator[]( size_type pos ) const
+        {
+            return this->_array[pos];
+        }
+
+        reference front()
+        {
+            return this->_array[0];
+        }
+
+        const_reference front() const
+        {
+            return this->_array[0];
+        }
+
+        reference back()
+        {
+            return this->_array[_c_idx -1];
+        }
+
+        const_reference back() const
+        {
+            return this->_array[_c_idx -1];
+        }
+
+        T* data()
+        {
+            return this->_array;
+        }
+
+        const T* data() const
+        {
+            return this->_array;
+        }
+
+        bool empty() const
+        {
+            return (_c_idx == 0);
+        }
+
+        size_type size() const
+        {
+            return _c_idx;
+        }
+
+
+        // size_type max_size() const
+        // {
+        //     return std::numeric_limits<value_type>::max() - 1;
+        // }
+        /** todo **/
+
+        void reserve( size_type new_cap )
+        {
+            
         }
 
     };
